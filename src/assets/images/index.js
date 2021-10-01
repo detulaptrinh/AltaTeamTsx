f7758324f4056fc7f6b72a8c097dfa568411af04
Trâm Anh <anhltps11760@fpt.edu.vn>
2021-10-01 16:26:48 +0700

    add

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,8 @@
 import AvatarSchool from './frame19.png';
 import AvatarTeacher from './frame20.png';
-
+import Avatar613 from "./frame613.png"
 export {
     AvatarSchool,
     AvatarTeacher,
+    Avatar613
 }
