7b5a45612748a821a42cb6135ff47f9dbb3fdfe0
TriPham <tri.pham1101@gmail.com>
2021-10-22 17:40:31 +0700

    tri add section 10

--- src/components/Tri_Page_10_11_12_13/Section_10_Student_Profile/StudentProfiles.tsx
+++ src/components/Tri_Page_10_11_12_13/Section_10_Student_Profile/StudentProfiles.tsx
@@ -15,6 +15,7 @@ import { PlusOutlined } from '@ant-design/icons';
 import Bonus from './Bonus';
 import SelectsBlock from '../Categories/SelectBlock';
 import SelectsYear from '../Categories/SelectsYear';
+import Disciplines from './Disciplines';
 const { TabPane } = Tabs;
 const { TextArea } = Input;
 
@@ -55,57 +56,15 @@ function StudentProfiles() {
                                         <Bonus/> 
                                     </TabPane>
                                     <TabPane tab="Kỉ luật" key="3">
-                                        Kỉ luật
+                                        <Disciplines/>
                                     </TabPane>
                                 </Tabs>
                             </div>
                         </Col>
-                        <Col span={5} className="studentProfile-headers-btn-group">
-                            <div className="studentProfile-headers-btn-group-add">
-                                <Button type="primary" shape="round" icon={<PlusOutlined />} onClick={() => setVisible(true)}>
-                                    Thêm mới
-                                </Button>
-                            </div>
-                        </Col>
+                        
                     </Row>
                 </div>
-                
-                <div className="bg-modal-update-bonus">
-                    <Modal className="bg-modal-update-bonus-modal-content"
-                        centered
-                        visible={visible}
-                        onOk={() => setVisible(false)}
-                        onCancel={() => setVisible(false)} >
-                        <Form>
-                            <h2 className="bg-modal-update-bonus-style-name-update-bonus">Cập nhật khen thưởng</h2>
-                            <Form.Item label="Học Viên" className="bg-modal-update-bonus-style-label">
-                                <p className="bg-modal-update-bonus-style-txt-name">Nguyễn Ngọc Tuyết</p>
-                            </Form.Item>
-                            <Form.Item label="Lớp hiện tại" className="bg-modal-update-bonus-style-label">
-                                <p className="bg-modal-update-bonus-style-txt-class">10A</p>
-                            </Form.Item>
-                            <Form.Item label="Ngày Khen Thưởng" className="bg-modal-update-bonus-style-label">
-                                <DatePicker className="bg-modal-update-bonus-style-date-picker" />
-                                <Button className="bg-modal-update-bonus-style-btn-semester">Học Kì I</Button>
-
-
-                            </Form.Item>
-                            <Form.Item label="Nội dung" className="bg-modal-update-bonus-style-label">
-                                <TextArea rows={4} className="bg-modal-update-bonus-style-input" />
-                            </Form.Item>
-
-                            <Form.Item label="Học kì I:" className="bg-modal-update-bonus-style-label">
-                                <Upload>
-                                    <Button className="bg-modal-update-bonus-modal-content-btn-update" >Chọn tệp tải lên...</Button>
-                                </Upload>,
-                            </Form.Item>
-
-                            <Button className="bg-modal-update-bonus-modal-content-btn-cancel" onClick={() => setVisible(false)}>Hủy</Button>
-                            <Button className="bg-modal-update-bonus-modal-content-btn-save">Lưu</Button>
-                        </Form>
-
-                    </Modal>
-                </div>
+            
 </div>
 
         </>
